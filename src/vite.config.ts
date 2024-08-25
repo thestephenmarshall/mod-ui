42567662eeebf8820d1b18264008d197c4f238de
Stephen <smarshall1980@gmail.com>
2024-08-25 15:13:21 -0400

    Re-organize Vite src

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: '../',
+    outDir: '../html',
   },
   plugins: [react()],
+  server: {
+    hmr: true,
+  }
 })
