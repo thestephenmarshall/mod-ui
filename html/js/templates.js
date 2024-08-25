4d3eb0ae2c5f9708233c5c0ac3474de45dadbd1f
Stephen <smarshall1980@gmail.com>
2024-08-25 12:14:20 -0400

    Init

--- html/js/templates.js
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: 2012-2023 MOD Audio UG
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-/*
-  This file won't be loaded. Instead, the server will deliver the TEMPLATES
-  variable, with all templates needed for the system.*/
